ee8660b94a056d5a2182b3364498c408563f6a21
Warit Prasartritha <doliolarzz@hotmail.com>
2020-02-16 02:47:03 +0700

    fix

--- experiments/conv.py
+++ experiments/conv.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os
+from argparse import ArgumentParser
 sys.path.insert(0, '../')
 import torch
 from torch.optim import lr_scheduler
@@ -9,71 +10,86 @@ from utils.train import k_train
 from global_config import global_config
 from collections import OrderedDict
 from models.convLSTM import ConvLSTM
-# from net_params import convlstm_encoder_params, convlstm_forecaster_params
 
-config = {
-    'DEVICE': torch.device('cuda:0'),
-    'IN_LEN': 5,
-    'OUT_LEN': 1,
-    'BATCH_SIZE': 2,
-}
+if __name__ == "__main__":
 
-k_fold = 1
-batch_size = config['BATCH_SIZE']
-max_iterations = 2
-test_iteration_interval = 1000
-test_and_save_checkpoint_iterations = 1000
-LR_step_size = 1000
-gamma = 0.7
-LR = 1e-4
-mse_loss = torch.nn.MSELoss().to(config['DEVICE'])
-IN_LEN = config['IN_LEN']
-OUT_LEN = config['OUT_LEN']
-convlstm_encoder_params = [
-    [
-        OrderedDict({'conv1_leaky_1': [1, 8, 7, 5, 1]}),
-        OrderedDict({'conv2_leaky_1': [64, 192, 5, 3, 1]}),
-        OrderedDict({'conv3_leaky_1': [192, 192, 3, 2, 1]}),
-    ],
+    parser = ArgumentParser()
+    parser.add_argument('--name', required=True)
+    parser.add_argument('--device', required=True)
+    parser.add_argument('--in', required=True)
+    parser.add_argument('--out', required=True)
+    parser.add_argument('--batchsize', required=True)
+    parser.add_argument('--multitask', required=True)
+    args = vars(parser.parse_args())
 
-    [
-        ConvLSTM(input_channel=8, num_filter=64, b_h_w=(batch_size, 96, 96),
-                 kernel_size=3, stride=1, padding=1, config=config),
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 32, 32),
-                 kernel_size=3, stride=1, padding=1, config=config),
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 16, 16),
-                 kernel_size=3, stride=1, padding=1, config=config),
+    if not os.path.exists('./logs'):
+        os.makedirs('./logs')
+
+    logs_name = './logs/logs_' + args['name']
+    multitask = True if args['multitask'].lower() == 'true' else False
+    config = {
+        'DEVICE': torch.device(args['device']),
+        'IN_LEN': int(args['in']),
+        'OUT_LEN': int(args['out']),
+        'BATCH_SIZE': int(args['batchsize']),
+    }
+
+    k_fold = 1
+    batch_size = config['BATCH_SIZE']
+    max_iterations = 2
+    test_iteration_interval = 1000
+    test_and_save_checkpoint_iterations = 1000
+    LR_step_size = 1000
+    gamma = 0.7
+    LR = 1e-4
+    mse_loss = torch.nn.MSELoss().to(config['DEVICE'])
+    IN_LEN = config['IN_LEN']
+    OUT_LEN = config['OUT_LEN']
+    convlstm_encoder_params = [
+        [
+            OrderedDict({'conv1_leaky_1': [1, 8, 7, 5, 1]}),
+            OrderedDict({'conv2_leaky_1': [64, 192, 5, 3, 1]}),
+            OrderedDict({'conv3_leaky_1': [192, 192, 3, 2, 1]}),
+        ],
+
+        [
+            ConvLSTM(input_channel=8, num_filter=64, b_h_w=(batch_size, 96, 96),
+                    kernel_size=3, stride=1, padding=1, config=config),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 32, 32),
+                    kernel_size=3, stride=1, padding=1, config=config),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 16, 16),
+                    kernel_size=3, stride=1, padding=1, config=config),
+        ]
     ]
-]
 
-convlstm_forecaster_params = [
-    [
-        OrderedDict({'deconv1_leaky_1': [192, 192, 4, 2, 1]}),
-        OrderedDict({'deconv2_leaky_1': [192, 64, 5, 3, 1]}),
-        OrderedDict({
-            'deconv3_leaky_1': [64, 8, 7, 5, 1],
-            'conv3_leaky_2': [8, 8, 3, 1, 1],
-            'conv3_3': [8, 1, 1, 1, 0]
-        }),
-    ],
+    convlstm_forecaster_params = [
+        [
+            OrderedDict({'deconv1_leaky_1': [192, 192, 4, 2, 1]}),
+            OrderedDict({'deconv2_leaky_1': [192, 64, 5, 3, 1]}),
+            OrderedDict({
+                'deconv3_leaky_1': [64, 8, 7, 5, 1],
+                'conv3_leaky_2': [8, 8, 3, 1, 1],
+                'conv3_3': [8, 1, 1, 1, 0]
+            }),
+        ],
 
-    [
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 16, 16),
-                 kernel_size=3, stride=1, padding=1, config=config),
-        ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 32, 32),
-                 kernel_size=3, stride=1, padding=1, config=config),
-        ConvLSTM(input_channel=64, num_filter=64, b_h_w=(batch_size, 96, 96),
-                 kernel_size=3, stride=1, padding=1, config=config),
+        [
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 16, 16),
+                    kernel_size=3, stride=1, padding=1, config=config),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(batch_size, 32, 32),
+                    kernel_size=3, stride=1, padding=1, config=config),
+            ConvLSTM(input_channel=64, num_filter=64, b_h_w=(batch_size, 96, 96),
+                    kernel_size=3, stride=1, padding=1, config=config),
+        ]
     ]
-]
 
-def get_model_set():
-    encoder = Encoder(convlstm_encoder_params[0], convlstm_encoder_params[1]).to(config['DEVICE'])
-    forecaster = Forecaster(convlstm_forecaster_params[0], convlstm_forecaster_params[1], config=config).to(config['DEVICE'])
-    encoder_forecaster = EF(encoder, forecaster).to(config['DEVICE'])
-    optimizer = torch.optim.Adam(encoder_forecaster.parameters(), lr=LR)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=LR_step_size, gamma=gamma)
-    return encoder_forecaster, optimizer, exp_lr_scheduler
+    def get_model_set():
+        encoder = Encoder(convlstm_encoder_params[0], convlstm_encoder_params[1]).to(config['DEVICE'])
+        forecaster = Forecaster(convlstm_forecaster_params[0], convlstm_forecaster_params[1], config=config).to(config['DEVICE'])
+        encoder_forecaster = EF(encoder, forecaster).to(config['DEVICE'])
+        optimizer = torch.optim.Adam(encoder_forecaster.parameters(), lr=LR)
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=LR_step_size, gamma=gamma)
+        return encoder_forecaster, optimizer, exp_lr_scheduler
 
-k_train(k_fold, get_model_set, mse_loss, 
-            batch_size, max_iterations, multitask=True, config=config)
+    k_train(k_fold, get_model_set, mse_loss, 
+                batch_size, max_iterations, save_dir=logs_name, multitask=multitask, config=config)
---
+++ experiments/conv_search.py
@@ -0,0 +1,25 @@
+import os
+from tqdm import tqdm
+
+device = 'cuda:1'
+in_len = [2, 5]
+out_len = [1, 18]
+multitask = [True, False]
+batch_size = 4
+
+with tqdm(total=len(in_len)*len(out_len)*len(multitask)) as pbar:
+    for il in in_len:
+        for ol in out_len:
+            for mt in multitask:
+                cmdStr = 'python3 conv.py --name={} --device={} --in={} --out={} --batchsize={} --multitask={}'.format(
+                    '_'.join([str(il), str(ol), str(batch_size), str(mt)]),
+                    device,
+                    il,
+                    ol,
+                    batch_size,
+                    mt
+                )
+                pbar.set_description('Executing:' + cmdStr)
+                os.system(cmdStr)
+                pbar.update(1)
+
