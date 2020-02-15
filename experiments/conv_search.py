6c6d3a100d622a2b6b69fd2b6f16cac3ac6e71c4
Warit Prasartritha <doliolarzz@hotmail.com>
2020-02-16 02:50:05 +0700

    fix

--- experiments/conv_search.py
+++ experiments/conv_search.py
@@ -7,19 +7,19 @@ out_len = [1, 18]
 multitask = [True, False]
 batch_size = 4
 
-with tqdm(total=len(in_len)*len(out_len)*len(multitask)) as pbar:
-    for il in in_len:
-        for ol in out_len:
-            for mt in multitask:
-                cmdStr = 'python3 conv.py --name={} --device={} --in={} --out={} --batchsize={} --multitask={}'.format(
-                    '_'.join([str(il), str(ol), str(batch_size), str(mt)]),
-                    device,
-                    il,
-                    ol,
-                    batch_size,
-                    mt
-                )
-                pbar.set_description('Executing:' + cmdStr)
-                os.system(cmdStr)
-                pbar.update(1)
+# with tqdm(total=len(in_len)*len(out_len)*len(multitask)) as pbar:
+for il in in_len:
+    for ol in out_len:
+        for mt in multitask:
+            cmdStr = 'python3 conv.py --name={} --device={} --in={} --out={} --batchsize={} --multitask={}'.format(
+                '_'.join([str(il), str(ol), str(batch_size), str(mt)]),
+                device,
+                il,
+                ol,
+                batch_size,
+                mt
+            )
+            # pbar.set_description('Executing:' + cmdStr)
+            os.system(cmdStr)
+            # pbar.update(1)
 
